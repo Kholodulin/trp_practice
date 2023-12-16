f50cc4860a60bb116e0596e57fd554b36ed15a60
Maxim Kholodulin <maxotron-0@list.ru>
2023-12-16 21:41:41 +0300

    craeted

--- MarkupLanguages/script.js
+++ MarkupLanguages/script.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+    // Загрузка данных из XML
+    $.ajax({
+        type: "GET",
+        url: "recipe.xml",
+        dataType: "xml",
+        success: function(xml) {
+            $(xml).find('recipe').each(function() {
+                var title = $(this).find('title').text();
+                var ingredients = $(this).find('ingredient').map(function() {
+                    return $(this).text();
+                }).get().join(', ');
+                var instructions = $(this).find('step').map(function() {
+                    return $(this).text();
+                }).get().join('<br>');
+
+                var recipeHtml = `
+                    <div class="recipe">
+                        <h2>${title}</h2>
+                        <p><strong>Ингредиенты:</strong> ${ingredients}</p>
+                        <p><strong>Инструкции:</strong><br> ${instructions}</p>
+                    </div>
+                `;
+                $('#recipes').append(recipeHtml);
+            });
+        }
+    });
+
     // Загрузка данных из JSON
     $.getJSON("recipe.json", function(data) {
         $.each(data.recipes, function(index, recipe) {
